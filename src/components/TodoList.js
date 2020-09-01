ba1a690bb2c2218cab3759ad108059f648c962a3
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-31 21:44:13 -0400

    added components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = (props) => {
+    
+    return (
+        <div> 
+        {props.datas.map((data) => (
+        <Todo
+          key={data.id}
+          list={data}
+          togglePurchased={props.togglePurchased}
+        />
+      ))}
+        </div>
+    )
+}
+
+export default TodoList
